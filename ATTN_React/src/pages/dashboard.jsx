a9880d0b786fcaff10a5d31741974efd80cb445d
Trishtian Capangpangan <ctrishtian@gmail.com>
2025-12-04 03:08:45 +0800

    Dashbaord changed "top-selling product" to "today's top-selling Product"

--- ATTN_React/src/pages/dashboard.jsx
+++ ATTN_React/src/pages/dashboard.jsx
@@ -198,7 +198,7 @@ export default function Dashboard() {
       {/* ===== STAT CARDS ===== */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="bg-white border-l-4 border-[#F8961E] p-4 rounded-md shadow-sm">
-          <p className="text-sm text-gray-500">Top-Selling Product</p>
+          <p className="text-sm text-gray-500">Today's Top-Selling Product</p>
           <h2 className="text-xl font-bold text-[#F8961E] mt-1">{topProduct}</h2>
         </div>
 
